72d069548f6dde875720f7be843547dd0651169a
svoby42 <qwerta009@gmail.com>
2021-11-30 12:09:53 +0100

    test 2

---
+++ src/test/java/svoby/spring/projectmanagement/controllers/HttpRequestTest.java
@@ -0,0 +1,29 @@
+package svoby.spring.projectmanagement.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void verzeSpravne_thenSuccess(){
+        String renderedHtml = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
+        assertEquals(renderedHtml.contains("0.0.1"), true);
+    }
+
+}
