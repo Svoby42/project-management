169907f6152c2247c1e1f26878780decef63bde1
svoby42 <qwerta009@gmail.com>
2021-12-01 11:13:42 +0100

    autentikace - role

--- src/main/java/svoby/spring/projectmanagement/security/SecurityConfig.java
+++ src/main/java/svoby/spring/projectmanagement/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,12 +17,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication().withUser("uzivatel").password("heslo").roles("USER")
-                .and().withUser("Pepa").password("heslo2").roles("USER");
+                .and().withUser("Pepa").password("heslo2").roles("USER")
+                .and().withUser("admin").password("heslo3").roles("ADMIN");
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeRequests()
+                .antMatchers("/projekty/novy").hasRole("ADMIN")
+                .antMatchers("/zamestnanci/novy").hasRole("ADMIN")
+                .antMatchers("/").authenticated().and().formLogin();
     }
 
 }
